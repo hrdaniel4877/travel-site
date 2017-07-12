fb2cd80c86c6670d030a755ba75c2a7ff8785291
Remus Daniel Heghedus <hrdaniel4877@gmail.com>
2017-07-12 18:15:35 -0400

    Install and setup browser-sync package; edit gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 nested = require('postcss-nested'),
-cssImport = require('postcss-import');
+cssImport = require('postcss-import'),
+browserSync = require('browser-sync').create();
 
 
 gulp.task('default', function() {
@@ -16,7 +17,7 @@ gulp.task('html', function() {
 });
 
 gulp.task('styles', function() {
-	gulp.src('./app/assets/styles/styles.css')
+	return gulp.src('./app/assets/styles/styles.css')
 	.pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
 	.pipe(gulp.dest('./app/temp/styles'));
 });
@@ -25,12 +26,25 @@ gulp.task('styles', function() {
 
 gulp.task('watch', function() {
 
+	browserSync.init({
+		//notify: false,
+		server: {
+			baseDir: 'app'
+		}
+	});
+
 	watch('./app/index.html', function() {
-		gulp.start('html');
+		browserSync.reload();
 	});
 
 	watch('./app/assets/styles/**/*.css', function() {
-		gulp.start('styles');
+		gulp.start('cssInject');
 	});
 
 });
+
+gulp.task('cssInject', ['styles'], function () {
+	return gulp.src('./app/temp/styles/styles.css')
+				.pipe(browserSync.stream());
+});
+
